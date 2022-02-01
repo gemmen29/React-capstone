7bbb70a2372e863ea337df51ae15f8e07c3ae3b6
George Magdy <georgtriple@gmail.com>
2022-02-01 19:42:01 +0200

    Display country details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRegions } from '../../redux/covid/covid';
 import styles from './Details.module.css';
 
 const Details = () => {
   const params = useParams();
-  return <div className={styles.Details}>{params.country_name}</div>;
+  const dispatch = useDispatch();
+  const regions = useSelector((state) => state.data.regions);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(getRegions(params.country_name));
+    }, 1000);
+  }, [dispatch]);
+
+  return (
+    <ul className={`${styles.Home} flex flex-col`}>
+      {regions.map((region) => (
+        <li key={region.name}>{`${region.name} ${region.today_confirmed}`}</li>
+      ))}
+    </ul>
+  );
 };
 
 Details.propTypes = {};
