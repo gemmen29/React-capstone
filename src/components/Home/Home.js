38b67bbc4d1441d5310dc8627f0bf2d96bbe48be
George Magdy <georgtriple@gmail.com>
2022-02-01 23:39:49 +0200

    Finish home style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { IoEarth } from 'react-icons/io5';
+import { GrMapLocation } from 'react-icons/gr';
 import { resetRegions } from '../../redux/covid/covid';
 import styles from './Home.module.css';
 
@@ -19,27 +21,48 @@ const Home = () => {
   }, [countries]);
 
   const searchHandler = (e) => {
-    const list = countries.filter((country) => country.name.toLowerCase().includes(e.target.value));
+    const list = countries.filter(
+      (
+        country, // eslint-disable
+      ) => country.name.toLowerCase().includes(e.target.value), // eslint-disable
+    );
     setDisplayedCountries(list);
   };
 
   return (
     <>
-      {`Total: ${totalNumber}`}
-      <br />
-      <input
-        type="text"
-        placeholder="Enter a country"
-        onChange={searchHandler}
-      />
-      <div className={`${styles.Home} grid grid-cols-2`}>
+      <div className="flex justify-around items-center bg-sky-500">
+        <IoEarth className="text-9xl" />
+        <div className="flex flex-col items-center text-2xl font-semibold text-white">
+          <span>All Countries Cases</span>
+          <span>{`Total: ${totalNumber.toLocaleString('en-US')}`}</span>
+        </div>
+      </div>
+      <div className="bg-sky-500 py-2 flex justify-center">
+        <input
+          type="text"
+          placeholder="Enter a country"
+          onChange={searchHandler}
+          className="shadow appearance-none border rounded w-4/12 py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div className="grid grid-cols-2">
         {displayedCountries.map((country) => (
           <NavLink
             key={country.name}
             to={`/${country.name}`}
             onClick={navlinkHandler}
+            className={`${styles.navlinks} flex flex-col items-center py-3 px-2 bg-sky-700`}
           >
-            {`${country.name} ${country.today_confirmed}`}
+            <GrMapLocation className="text-8xl text-center" />
+            <div className="ml-auto text-white flex flex-col gap-2 text-3xl font-bold items-end">
+              <span className="flex flex-col items-end">
+                {country.name.split(' ').map((piece) => (
+                  <span key={piece}>{piece}</span>
+                ))}
+              </span>
+              <span>{country.today_confirmed.toLocaleString('en-US')}</span>
+            </div>
           </NavLink>
         ))}
       </div>
