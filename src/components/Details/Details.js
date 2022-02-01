8206d8537477ea752246288cf11be64eac46d101
George Magdy <georgtriple@gmail.com>
2022-02-02 00:44:57 +0200

    Finish details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
+import { IoEarth } from 'react-icons/io5';
 import { getRegions } from '../../redux/covid/covid';
-import styles from './Details.module.css';
 
 const Details = () => {
   const params = useParams();
@@ -17,11 +17,39 @@ const Details = () => {
   }, [dispatch]);
 
   return (
-    <ul className={`${styles.Home} flex flex-col`}>
-      {regions.map((region) => (
-        <li key={region.name}>{`${region.name} ${region.today_confirmed}`}</li>
-      ))}
-    </ul>
+    <>
+      <div className="flex justify-around items-center py-6 bg-sky-500">
+        <IoEarth className="text-9xl" />
+        <div className="flex flex-col items-end text-3xl font-semibold text-white">
+          <span>
+            <span className="flex flex-col items-end">
+              {params.country_name.split(' ').map((piece) => (
+                <span key={piece}>{piece}</span>
+              ))}
+            </span>
+          </span>
+          <span>{`${regions.total.toLocaleString('en-US')} cases`}</span>
+        </div>
+      </div>
+      <div className="bg-sky-700 text-lg px-5 py-1 text-white">
+        Country Breakdown - 2022
+      </div>
+      <ul className="flex flex-col">
+        {regions.regions.map((region) => (
+          <li
+            key={region.name}
+            className="flex justify-between items-center px-6 py-6 even:bg-sky-700 text-white text-3xl text-semibold"
+          >
+            <span className="flex flex-col items-start">
+              {region.name.split(' ').map((piece) => (
+                <span key={piece}>{piece}</span>
+              ))}
+            </span>
+            <span>{region.today_confirmed.toLocaleString('en-US')}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
