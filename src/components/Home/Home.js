be4b3315af770c1a18c3a077db78e5023fbfd552
George Magdy <georgtriple@gmail.com>
2022-02-01 10:37:02 +0200

    First display country version

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import styles from './Home.module.css';
 
-const Home = () => (
-  <div className={styles.Home}>
-    <NavLink to={`/${'Test'}`}>Details</NavLink>
-  </div>
-);
+const Home = () => {
+  const countries = useSelector((state) => state.countries);
+  console.log(countries);
+
+  return (
+    <div className={`${styles.Home} grid grid-cols-2`}>
+      {Object.keys(countries).map((country) => (
+        <NavLink key={country} to={`/${country}`}>
+          {country}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
 
 Home.propTypes = {};
 
