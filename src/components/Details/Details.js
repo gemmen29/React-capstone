8972e748fc9eee982050d0d707fe2eee4dabf60f
George Magdy <georgtriple@gmail.com>
2022-02-01 11:01:52 +0200

    Get params from URL

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import styles from './Details.module.css';
 
-const Details = () => <div className={styles.Details}>Details Component</div>;
+const Details = () => {
+  const params = useParams();
+  return <div className={styles.Details}>{params.country_name}</div>;
+};
 
 Details.propTypes = {};
 
