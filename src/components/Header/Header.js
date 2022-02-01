4dcf53dd7a6fac81b2d103ad7102074cb4e2c5dc
George Magdy <georgtriple@gmail.com>
2022-02-01 22:13:02 +0200

    Finish header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import styles from './Header.module.css';
+import { BsFillMicFill } from 'react-icons/bs';
+import { IoIosSettings } from 'react-icons/io';
 
-const Header = () => <div className={styles.Header}>Header Component</div>;
+const Header = () => (
+  <header className="bg-sky-800 text-white">
+    <nav className="flex justify-between items-center py-2 px-5">
+      <div>2022</div>
+      <div>All Cases</div>
+      <div className="flex gap-3">
+        <BsFillMicFill />
+        <IoIosSettings />
+      </div>
+    </nav>
+  </header>
+);
 
 Header.propTypes = {};
 
