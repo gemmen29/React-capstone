580e16c2be7ffb0bb8781888dd66e477ed111645
George Magdy <georgtriple@gmail.com>
2022-02-02 14:29:11 +0200

    Fix linters-errors

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,12 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import configureStore from 'redux-mock-store';
 import Header from '../components/Header/Header';
-
-const totalCountries = 10000000;
 
 describe('Home component', () => {
   test('Test Rendering countries', () => {
