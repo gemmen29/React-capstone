f13ac7237e938224157215e2bdcd51ef6254cc70
George Magdy <georgtriple@gmail.com>
2022-02-02 15:42:57 +0200

    Use spinner in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
+import ClipLoader from 'react-spinners/ClipLoader';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { IoEarth } from 'react-icons/io5';
 import { GrMapLocation } from 'react-icons/gr';
+import { css } from '@emotion/react';
 import { resetRegions } from '../../redux/covid/covid';
 import styles from './Home.module.css';
 
@@ -15,6 +16,12 @@ const Home = () => {
   const navlinkHandler = () => {
     dispatch(resetRegions());
   };
+
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: white;
+  `;
 
   useEffect(() => {
     setDisplayedCountries(countries);
@@ -35,47 +42,58 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex justify-around items-center bg-sky-500">
-        <IoEarth className="text-9xl" />
-        <div className="flex flex-col items-center text-2xl font-semibold text-white">
-          <span>All Countries Cases</span>
-          <span>{`Total: ${totalNumber.toLocaleString('en-US')}`}</span>
+      {countries.length === 0 && (
+        <div className="flex justify-center items-center min-w-[100vw] min-h-[100vh]">
+          <ClipLoader color="white" css={override} size={150} />
         </div>
-      </div>
-      <div className="bg-sky-500 py-2 flex justify-center">
-        <input
-          type="text"
-          placeholder="Enter a country"
-          onChange={searchHandler}
-          className="shadow appearance-none border rounded w-4/12 py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <ul className="grid grid-cols-2">
-        {displayedCountries.map((country) => (
-          <li
-            data-testid="listitem"
-            key={country.name}
-            className={`${styles.navlinks} py-3 px-2 bg-sky-700`}
-          >
-            <NavLink
-              key={country.name}
-              to={`/${country.name}`}
-              onClick={navlinkHandler}
-              className="w-full flex flex-col items-center"
-            >
-              <GrMapLocation className="text-8xl text-center" />
-              <div className="ml-auto text-white flex flex-col gap-2 text-3xl font-bold items-end">
-                <span className="flex flex-col items-end">
-                  {country.name.split(' ').map((piece) => (
-                    <span key={piece}>{piece}</span>
-                  ))}
-                </span>
-                <span>{country.today_confirmed.toLocaleString('en-US')}</span>
-              </div>
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+      )}
+      {countries.length > 0 && (
+        <>
+          <div className="flex justify-around items-center bg-sky-500">
+            <IoEarth className="text-9xl" />
+            <div className="flex flex-col items-center text-2xl font-semibold text-white">
+              <span>All Countries Cases</span>
+              <span>{`Total: ${totalNumber.toLocaleString('en-US')}`}</span>
+            </div>
+          </div>
+          <div className="bg-sky-500 py-2 flex justify-center">
+            <input
+              type="text"
+              placeholder="Enter a country"
+              onChange={searchHandler}
+              className="shadow appearance-none border rounded w-4/12 py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <ul className="grid grid-cols-2">
+            {displayedCountries.map((country) => (
+              <li
+                data-testid="listitem"
+                key={country.name}
+                className={`${styles.navlinks} py-3 px-2 bg-sky-700`}
+              >
+                <NavLink
+                  key={country.name}
+                  to={`/${country.name}`}
+                  onClick={navlinkHandler}
+                  className="w-full flex flex-col items-center"
+                >
+                  <GrMapLocation className="text-8xl text-center" />
+                  <div className="ml-auto text-white flex flex-col gap-2 text-3xl font-bold items-end">
+                    <span className="flex flex-col items-end">
+                      {country.name.split(' ').map((piece) => (
+                        <span key={piece}>{piece}</span>
+                      ))}
+                    </span>
+                    <span>
+                      {country.today_confirmed.toLocaleString('en-US')}
+                    </span>
+                  </div>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 };
