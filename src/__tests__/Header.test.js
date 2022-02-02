570ddfb8b43b6ede4bd4c57e003dcb0408d35d36
George Magdy <georgtriple@gmail.com>
2022-02-02 13:40:56 +0200

    Finish header test

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import configureStore from 'redux-mock-store';
+import Header from '../components/Header/Header';
+
+const totalCountries = 10000000;
+
+describe('Home component', () => {
+  test('Test Rendering countries', () => {
+    const intialState = {
+      show: true,
+    };
+
+    const middlewares = [thunk, logger];
+    const mockStore = configureStore(middlewares);
+    const store = mockStore({ data: intialState });
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
